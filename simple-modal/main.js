be6c2cd72e44461e969d26b2f8d08d86d7d30b53
Ulises Gomez <ulisesgomez120@gmail.com>
2019-08-10 10:52:34 -0700

    added a simple modal created with vanilla HTML, CSS, Javascript

---
+++ simple-modal/main.js
@@ -0,0 +1,19 @@
+const modal = document.querySelector("#simpleModal");
+const modalBtn = document.querySelector("#modalBtn");
+const closeBtn = document.querySelector(".closeBtn");
+
+modalBtn.addEventListener("click", openModal);
+closeBtn.addEventListener("click", closeModal);
+window.addEventListener("click", outsideClick);
+
+function openModal() {
+  modal.style.display = "block";
+}
+function closeModal() {
+  modal.style.display = "none";
+}
+function outsideClick(e) {
+  if (e.target == modal) {
+    modal.style.display = "none";
+  }
+}
