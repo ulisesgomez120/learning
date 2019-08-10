00cccc564049ebea730db31584ecafe36a841c99
Ulises Gomez <ulisesgomez120@gmail.com>
2019-08-10 15:33:03 -0700

    added iframe to modal

--- simple-modal/main.js
+++ simple-modal/main.js
@@ -3,16 +3,17 @@ const modalBtn = document.querySelector("#modalBtn");
 const closeBtn = document.querySelector(".closeBtn");
 
 modalBtn.addEventListener("click", openModal);
-closeBtn.addEventListener("click", closeModal);
+// closeBtn.addEventListener("click", closeModal);
 window.addEventListener("click", outsideClick);
 
 function openModal() {
-  modal.style.display = "block";
+  modal.style.display = "grid";
 }
-function closeModal() {
-  modal.style.display = "none";
-}
+// function closeModal() {
+//   modal.style.display = "none";
+// }
 function outsideClick(e) {
+  console.log(e.target);
   if (e.target == modal) {
     modal.style.display = "none";
   }
